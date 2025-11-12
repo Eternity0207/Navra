7914572a2cf8cd92c7796d1ba1d255dcb3ae36c3
husain537404 <husain537539@gmail.com>
2025-11-12 20:07:46 +0530

    added functions for dijkstra in algorithms header file

--- backend/include/algorithms.h
+++ backend/include/algorithms.h
@@ -6,14 +6,15 @@
 #include <utility>
 #include <unordered_set>
 
+std::vector<double> dijkstra(const Graph& g,int start);
+std::pair<std::vector<double>,std::vector<int>>dijkstraWithPath(const Graph& g,int start);
+std::vector<int> reconstructPath(const std::vector<int>& parent,int start,int end);
+
 std::vector<int> aStarPath(const Graph& g,int start,int goal);
 double haversine(double lat1,double lon1,double lat2,double lon2);
 
-std::pair<double,std::vector<int>>
-    tspDP(const std::vector<std::vector<double>>& dist);
-std::pair<double,std::vector<int>>
-    greedyTSP(const Graph& g,int start,const std::vector<int>& mustVisit);
-void twoOptImprovement(std::vector<int>& tour,
-                       const std::vector<std::vector<double>>& dist);
+std::pair<double,std::vector<int>>tspDP(const std::vector<std::vector<double>>& dist);
+std::pair<double,std::vector<int>>greedyTSP(const Graph& g,int start,const std::vector<int>& mustVisit);
+void twoOptImprovement(std::vector<int>& tour,const std::vector<std::vector<double>>& dist);
 
 #endif

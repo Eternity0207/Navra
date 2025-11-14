caf24a408453c0ca84b6f1f86449e4257f49b46a
Mani <77221000+Eternity0207@users.noreply.github.com>
2025-11-14 20:51:17 +0530

    Added Linux Version of Executable File

--- website/vite.config.js
+++ website/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -12,4 +13,4 @@ export default defineConfig({
       }
     }
   }
-})
+});
